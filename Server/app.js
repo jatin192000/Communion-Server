f67ea3c1bbe77be444ba4221d67f8b1839d4121a
jatin192000 <jatin192000@gmail.com>
2021-06-04 18:42:38 +0530

    minor

--- Server/app.js
+++ Server/app.js
@@ -15,6 +15,7 @@ mongoose.connect(
 		useNewUrlParser: true,
 		useUnifiedTopology: true,
 		useFindAndModify: false,
+		useCreateIndex: true,
 	},
 	() => {
 		console.log("successfully connected to database");
